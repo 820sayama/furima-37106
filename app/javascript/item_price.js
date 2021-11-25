1f07fc19ce34b40a2f216444a0fa0866690c69b6
820sayama <sayama380820@gmail.com>
2021-11-25 12:26:27 +0900

    修正3

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(priceInput);
     const priceInput2 = document.getElementById("add-tax-price"); 
     price = inputValue * 0.1
   priceInput2.innerHTML = Math.floor(price);
