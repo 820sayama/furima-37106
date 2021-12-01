6ef629ff6f08cac8f83587423c407d16b3dea444
820sayama <sayama380820@gmail.com>
2021-12-01 17:57:45 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
 
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:encrypted_password,:nickname,:first_name,:last_name,:first_name_kana,:last_name_kana,:birthday_day])
     end
